882851d8413f0a1dc527a362f04ccfb580dcc1cd
Alejandro <rodrigueznatalandy@gmail.com>
2021-02-11 14:07:08 -0400

    Added YAML file for CI/CD in GitHub

--- GUI/VanGogh/app/src/test/java/com/example/vangogh/ExampleUnitTest.java
+++ GUI/VanGogh/app/src/test/java/com/example/vangogh/ExampleUnitTest.java
@@ -19,6 +19,7 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+    
 
     PreProcessor pp = new PreProcessor();
 
