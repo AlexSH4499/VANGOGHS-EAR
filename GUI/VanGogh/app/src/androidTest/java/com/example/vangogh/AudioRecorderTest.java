299ef654f6cd4a435eaaccc195b00eaeeb514df3
AlexSH4499 <rodrigueznatalandy@gmail.com>
2021-02-26 16:46:16 -0400

    Added FIXED Espresso dependencies

--- GUI/VanGogh/app/src/androidTest/java/com/example/vangogh/AudioRecorderTest.java
+++ GUI/VanGogh/app/src/androidTest/java/com/example/vangogh/AudioRecorderTest.java
@@ -1,8 +1,18 @@
 package com.example.vangogh;
 //import androidx.test.filters.LargeTest;
 
-import android.test.suitebuilder.annotation.LargeTest;
+import android.provider.MediaStore;
 
+import androidx.lifecycle.Lifecycle;
+import androidx.test.filters.LargeTest;
+import androidx.test.runner.AndroidJUnit4;
+
+//import androidx.test.ext.junit.rules.ActivityScenarioRule;
+//import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.fragment.app.testing.FragmentScenario;
+import androidx.test.core.app.ActivityScenario;
+
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
@@ -11,8 +21,18 @@ import org.junit.runner.RunWith;
 
 //import androidx.test.ext.junit.rules.ActivityScenarioRule;
 //import androidx.test.ext.junit.runners.AndroidJUnit4;
-
+//import androidx.fragment.app.testing.launchFragmentInContainer;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 //import static android.support.test.espresso.Espresso.onData;
 //import static android.support.test.espresso.Espresso.onView;
 //import static android.support.test.espresso.action.ViewActions.click;
@@ -24,7 +44,7 @@ import static org.junit.Assert.assertEquals;
 //import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 
-
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -32,65 +52,130 @@ import static org.junit.Assert.assertEquals;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 
-//@RunWith(AndroidJUnit4.class)
+@RunWith(AndroidJUnit4ClassRunner.class)
 @LargeTest
 public class AudioRecorderTest {
 
     AudioRecorder audio_rec;
     private String emptyOutputFilePath;
-
+    FragmentScenario audioRecorder;
 //    @Rule
-//    public ActivityScenarioRule<MainActivity> activityRule = new ActivityScenarioRule<>(MainActivity.class);
-//    @Rule
-//    public ActivityScenarioRule<MainActivity> activityRule
-//                    = new ActivityScenarioRule<>(MainActivity.class);
+//    public ActivityScenario<MainActivity> activityRule =  ActivityScenario.launch(MainActivity.class);
 
     @Before
-    public void setEmptyOutputFilePath()
+    public void setUpActivityAndFragment()
     {
-        emptyOutputFilePath = "";
+//        ActivityScenario.launch(MainActivity.class).onActivity(activity -> {activity.setFragment()})
+       audioRecorder = FragmentScenario.launchInContainer(AudioRecorder.class);
     }
+
+//    @Before
+//    public void setEmptyOutputFilePath()
+//    {
+//        emptyOutputFilePath = "";
+//    }
 
 
     @Test
     public void openAudioFragment_StartRecording()
     {
+        audioRecorder = FragmentScenario.launchInContainer(AudioRecorder.class);
+        audioRecorder.recreate();
+//        FragmentScenario.launchInContainer(AudioRecorder.class);
+        audioRecorder.moveToState(Lifecycle.State.STARTED);
+        //Verify Mic Record Button is displayed
+        onView(withId(R.id.microphone_button)).check(matches(isDisplayed()));
 
+        //Verify button is clickable
+        onView(withId(R.id.microphone_button)).check(matches(isClickable()));
+
+        //Verify button has "Start" string
+        onView(withId(R.id.microphone_button)).check(matches(withText("Start")));
+
+//        Perform click here
+        onView(withId(R.id.microphone_button)).perform(click());
+
+        //Check Toasts!
+        //TODO: Implement Activity Scenario to test this
+        //Perform click here
+        onView(withId(R.id.microphone_button)).perform(click());
+    }
+
+    @Test
+    public void openAudioFragment_StopRecording()
+    {
+        FragmentScenario.launchInContainer(AudioRecorder.class);
+
+        //Verify Mic Record Button is displayed
+        onView(withId(R.id.microphone_button)).check(matches(isDisplayed()));
+
+        //Verify button is clickable
+        onView(withId(R.id.microphone_button)).check(matches(isClickable()));
+
+//        //Verify button has "Start" string
+//        onView(withId(R.id.microphone_button)).check(matches(withText("Start")));
+
+        //Perform click here
+        onView(withId(R.id.microphone_button)).perform(click());
+
+        //Verify Mic Record Button is displayed
+        onView(withId(R.id.microphone_button)).check(matches(isDisplayed()));
+
+        //Verify button is clickable
+        onView(withId(R.id.microphone_button)).check(matches(isClickable()));
+
+//        //Verify button has "Start" string
+//        onView(withId(R.id.microphone_button)).check(matches(withText("Stop")));
+
+        //Perform click here
+        onView(withId(R.id.microphone_button)).perform(click());
+
+        //Check Toasts!
+        //TODO: Implement Activity Scenario to test this
     }
 
 
     //this wont pass because we cant use the Activity Context since we're not running the entire app
-    @Test
-    public void empty_filename_file_extension_isCorrect()
-    {
-        //This fails because we cant instance a Fragment for the context
+//    @Test
+//    public void empty_filename_file_extension_isCorrect()
+//    {
+//        //This fails because we cant instance a Fragment for the context
 //        onView(withId(R.id.new_audio_fragment_container_view));
 //        audio_rec = new AudioRecorder();
 //        String expected_file_output = "sample.3gp";
 //        String test = audio_rec.OutputFilePath("");
 //        assertEquals(expected_file_output, test);
 //        audio_rec = null;
+//    }
+
+//    @Test
+//    public void nonEmptyString() {
+//    }
+
+
+//    @Test
+//    public void onRequestPermissionsResult() {
+//    }
+
+//    @Test
+//    public void outputFilePath() {
+//    }
+//
+//    @Test
+//    public void testNonEmptyString() {
+//    }
+
+    @Test
+    public void testOnCreateView() {
     }
 
     @Test
-    public void outputFilePath() {
+    public void testOnDestroyView() {
     }
 
-    @Test
-    public void nonEmptyString() {
-    }
-
-    @Test
-    public void onCreateView() {
-    }
-
-    @Test
-    public void onDestroyView() {
-    }
-
-    @Test
-    public void onRequestPermissionsResult() {
-    }
+//    @Test
+//    public void testOnRequestPermissionsResult() {
+//    }
 //
 //    @Test
 //    public void non_empty_filename_file_extension_isCorrect()
--- GUI/VanGogh/app/src/androidTest/java/com/example/vangogh/ChordFragmentTest.java
+++ GUI/VanGogh/app/src/androidTest/java/com/example/vangogh/ChordFragmentTest.java
@@ -4,8 +4,22 @@ import com.example.vangogh.ChordFragment;
 import android.os.Bundle;
 import android.test.suitebuilder.annotation.LargeTest;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 //import androidx.fragment.app.testing.FragmentScenario;
 //import androidx.test.filters.LargeTest;
+
+import androidx.annotation.ContentView;
+import androidx.fragment.app.testing.FragmentScenario;
+import androidx.lifecycle.Lifecycle;
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -21,7 +35,7 @@ import junit.extensions.ActiveTestSuite;
 
 import static org.junit.Assert.assertEquals;
 
-//@RunWith(AndroidJUnit4.class)
+@RunWith(AndroidJUnit4ClassRunner.class)
 @LargeTest
 public class ChordFragmentTest {
 
@@ -34,36 +48,44 @@ public class ChordFragmentTest {
     @Test
     public void testSearchForFile() {
         Bundle args = new Bundle();
-//         FragmentScenario<ChordFragment> fragment_in_container = launchFragment<ChordFragment>();
+//         FragmentScenario<ChordFragment> fragment_in_container = FragmentScenario.launchInContainer(ChordFragment.class);
     }
 
     @Test
-    public void testOnCreateView() {
+    public void test_OnCreate_PutChord()
+    {
+        FragmentScenario<ChordFragment> fragment_in_container = FragmentScenario.launchInContainer(ChordFragment.class);
+        fragment_in_container.moveToState(Lifecycle.State.STARTED);
+        onView(withId(R.id.chord_input)).check(matches(isDisplayed())).perform(typeText("A"));
     }
 
-    @Test
-    public void testGetCurrentChord() {
-    }
-
-    @Test
-    public void testOnActivityResult() {
-    }
-
-    @Test
-    public void searchForFile() {
-    }
-
-    @Test
-    public void onCreateView() {
-    }
-
-    @Test
-    public void getCurrentChord() {
-    }
-
-    @Test
-    public void onActivityResult() {
-    }
+//    @Test
+//    public void testOnCreateView() {
+//    }
+//
+//    @Test
+//    public void testGetCurrentChord() {
+//    }
+//
+//    @Test
+//    public void testOnActivityResult() {
+//    }
+//
+//    @Test
+//    public void searchForFile() {
+//    }
+//
+//    @Test
+//    public void onCreateView() {
+//    }
+//
+//    @Test
+//    public void getCurrentChord() {
+//    }
+//
+//    @Test
+//    public void onActivityResult() {
+//    }
 
     private ChordFragment startChordFragment()
     {
